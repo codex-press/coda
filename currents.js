6fcf1f9514e21e7fc16b8cfd7234821b8f1fbc3f
Tim Farnam <twfarnam@gmail.com>
2016-07-23 01:59:49 -0400

    had to comment out js so it would compile... sad

--- currents.js
+++ currents.js
@@ -1,61 +1,61 @@
-// Currents page needs special codes....
+// // Currents page needs special codes....
 
-var currentsEvents = {
-  'click .icon-angle' : currentsPager,
-  'wheel .index'      : currentsScroller,
-};
+// var currentsEvents = {
+//   'click .icon-angle' : currentsPager,
+//   'wheel .index'      : currentsScroller,
+// };
 
-article.hook(currentsEvents, article.el);
-article.hook({resize: updateArrows});
-updateArrows();
-
-
-function updateArrows() {
-  currentsArticle.select('.current').each(function(current) {
-    var index = current.select('.index').first();
-
-    if (index.scrollWidth - index.clientWidth - index.scrollLeft > 120)
-      codex.dom(current).addClass('overflow-right');
-    else
-      codex.dom(current).removeClass('overflow-right');
-
-    if (index.scrollLeft > 0)
-      codex.dom(current).addClass('overflow-left');
-    else
-      codex.dom(current).removeClass('overflow-left');
-
-  });
-};
+// article.hook(currentsEvents, article.el);
+// article.hook({resize: updateArrows});
+// updateArrows();
 
 
-function currentsScroller(e) {
-  codex.dom(e.target).closest('.index').scrollX(e.deltaX);
-  updateArrows();
-  if (!e.deltaY || (e.deltaX / e.deltaY) > .8)
-    e.preventDefault();
-};
+// function updateArrows() {
+//   currentsArticle.select('.current').each(function(current) {
+//     var index = current.select('.index').first();
+
+//     if (index.scrollWidth - index.clientWidth - index.scrollLeft > 120)
+//       codex.dom(current).addClass('overflow-right');
+//     else
+//       codex.dom(current).removeClass('overflow-right');
+
+//     if (index.scrollLeft > 0)
+//       codex.dom(current).addClass('overflow-left');
+//     else
+//       codex.dom(current).removeClass('overflow-left');
+
+//   });
+// };
 
 
-function currentsPager(e) {
-  e.preventDefault();
+// function currentsScroller(e) {
+//   codex.dom(e.target).closest('.index').scrollX(e.deltaX);
+//   updateArrows();
+//   if (!e.deltaY || (e.deltaX / e.deltaY) > .8)
+//     e.preventDefault();
+// };
 
-  var index = codex.dom(e.target).closest('.current').select('.index').first();
 
-  var direction = codex.dom(e.target).is('.left') ? -1 : 1;
-  var jump = window.innerWidth * .8 * direction;
-  var start = index.scrollLeft;
-  var max = index.scrollWidth - index.clientWidth;
-  var change = constrain(jump, start * -1, max - start);
-  var duration = Math.max(100, 500 * (change/jump));
+// function currentsPager(e) {
+//   e.preventDefault();
 
-  var ease = codex.animate.cubicOut(start, change, duration);
+//   var index = codex.dom(e.target).closest('.current').select('.index').first();
 
-  codex.animate(
-    duration: duration,
-    tick: function(time) { index.scrollLeft = Math.round(ease(time)); },
-    done: updateArrows
-  );
+//   var direction = codex.dom(e.target).is('.left') ? -1 : 1;
+//   var jump = window.innerWidth * .8 * direction;
+//   var start = index.scrollLeft;
+//   var max = index.scrollWidth - index.clientWidth;
+//   var change = constrain(jump, start * -1, max - start);
+//   var duration = Math.max(100, 500 * (change/jump));
 
-};
+//   var ease = codex.animate.cubicOut(start, change, duration);
+
+//   codex.animate(
+//     duration: duration,
+//     tick: function(time) { index.scrollLeft = Math.round(ease(time)); },
+//     done: updateArrows
+//   );
+
+// };
 
 
--- header.js
+++ header.js
@@ -1,32 +1,32 @@
-// import {compile, unscopeLinks}  from './template_helpers.es6';
-// import {log, constrain, alphanum} from './utility.es6';
-// import player     from './player.es6';
-// import dom        from './dom.es6';
-// import animate    from './easing.es6';
+// // import {compile, unscopeLinks}  from './template_helpers.es6';
+// // import {log, constrain, alphanum} from './utility.es6';
+// // import player     from './player.es6';
+// // import dom        from './dom.es6';
+// // import animate    from './easing.es6';
 
-var init = function init() {
+// var init = function init() {
 
-  player.on('navigate', updateHeader);
+//   player.on('navigate', updateHeader);
 
-  player.on('video:play', v => {
-    siteChrome.find('header').transform({y: '-100%'});
-  });
+//   player.on('video:play', v => {
+//     siteChrome.find('header').transform({y: '-100%'});
+//   });
 
-  player.on('video:pause', v => {
-    siteChrome.find('header').transform({y: ''});
-  });
+//   player.on('video:pause', v => {
+//     siteChrome.find('header').transform({y: ''});
+//   });
 
-};
+// };
 
 
-var updateHeader = function updateHeader(article) {
-  siteChrome.select('header a.target').removeClass('target');
+// var updateHeader = function updateHeader(article) {
+//   siteChrome.select('header a.target').removeClass('target');
 
-  if (article.attrs.url === '/coda')
-    siteChrome.find('.navigation-links a[href="/coda"]').addClass('target');
-  else if (article.attrs.url === '/coda/currents')
-    siteChrome.find('a[href="/coda/currents"]').addClass('target');
-  else if (article.attrs.url === '/coda/about')
-    siteChrome.find('a[href="/coda/about"]').addClass('target');
-};
+//   if (article.attrs.url === '/coda')
+//     siteChrome.find('.navigation-links a[href="/coda"]').addClass('target');
+//   else if (article.attrs.url === '/coda/currents')
+//     siteChrome.find('a[href="/coda/currents"]').addClass('target');
+//   else if (article.attrs.url === '/coda/about')
+//     siteChrome.find('a[href="/coda/about"]').addClass('target');
+// };
 
