8f643663dd746c97d56e09e1af9d260fd5862978
Tim Farnam <twfarnam@gmail.com>
2016-08-08 15:07:15 -0400

    renames current_single to single_current

--- homepage.js
+++ homepage.js
@@ -4,7 +4,7 @@
   var article = require('article').default;
 
   article.once('ready', function() {
-    let el = document.getElementById("dismiss");
+    var el = document.getElementById("dismiss");
     
     el.addEventListener('click', function() {
       document.getElementById("pilot_notification").classList.add("dismissed");
