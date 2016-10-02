5f3e4685315d6119313fb48b9974d3c352057fd3
neal.zupancic <neal.zupancic@gmail.com>
2016-10-02 18:33:18 +0400

    fix bugs in current js

--- currents1.js
+++ currents1.js
@@ -52,29 +52,33 @@
 
 //current expansion selector
   function currentsExpander(e) {
+    //remove all other .top-expansion classes
+    article.select('.expansion').each(function(expansion) {
+      dom(expansion).removeClass('top-expansion');
+    });
 
-
-
-
-    if (e.target.tagName == "DIV"){
-    //  /*
-      //remove all other .top-expansion classes
-      article.select('.expansion').each(function(expansion) {
-        dom(expansion).removeClass('top-expansion');
-      });
-
-      article.select('.titles div').each(function(title) {
-        dom(title).removeClass('selected-title');
-      });
-    //  */
-      var currentName = e.target.className;
-      //console.log(currentName);
-
-      //add .top-expansion class to the current expansion
-      article.select('.expansion.' + currentName).addClass('top-expansion');
-
-      article.select('.titles .' + currentName).addClass('selected-title');
+    article.select('.titles div').each(function(title) {
+      dom(title).removeClass('selected-title');
+    });
+    var currentName = "";
+    //get the className of the target div
+    //since currentTarget doesn't seem to work, we need to check whether the
+    //target div is this element or its parent or grandparent
+    if (e.target.tagName == "DIV") {
+      currentName = e.target.className;
     }
+    else if ((e.target.tagName == "P") || (e.target.tagName == "H1")) {
+      console.log(e.target.tagName);
+      currentName = e.target.parentElement.className;
+    }
+    else if (e.target.tagName == "A") {
+      currentName = e.target.parentElement.parentElement.className;
+    }
+
+    //add .top-expansion class to the current expansion
+    article.select('.expansion.' + currentName).addClass('top-expansion');
+    //add .selected-title class to the current current title
+    article.select('.titles .' + currentName).addClass('selected-title');
   };
 
 
