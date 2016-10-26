af548eb80e38dca657162027237b21a0487f3aab
neal.zupancic <neal.zupancic@gmail.com>
2016-10-26 14:34:19 +0400

    js dev testing

--- normal_story_dev.js
+++ normal_story_dev.js
@@ -2,11 +2,8 @@ import dom from 'dom';
 import article from 'article';
 
 article.ready.then(() => {
-  console.log("dev testing successful, article ready");
+  console.log("testing successful, article ready");
 
 
 
-
-
-  });
-})
+});
